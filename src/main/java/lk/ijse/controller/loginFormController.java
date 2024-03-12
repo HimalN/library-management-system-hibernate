ebdf40abd2769c611a5084a82039a3cd6af3e9b3
HimalN <himalnimsara888@gmail.com>
2024-03-12 14:43:12 +0530

    ->Login Form Ui ->SignUp Form Ui

--- src/main/java/lk/ijse/controller/loginFormController.java
+++ src/main/java/lk/ijse/controller/loginFormController.java
@@ -1,4 +1,42 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class loginFormController {
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private Button btnSignUp;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSignUpOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtPasswordOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtUsernameOnAction(ActionEvent event) {
+
+    }
+
 }
--- src/main/java/lk/ijse/controller/signUpFormController.java
+++ src/main/java/lk/ijse/controller/signUpFormController.java
@@ -1,4 +1,50 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class signUpFormController {
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private Button btnSignUp;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSignUpOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtNameOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtPasswordOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtUsernameOnAction(ActionEvent event) {
+
+    }
+
 }
