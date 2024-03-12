84c5ffdf518b14ea7b7d1d15230b5649a9152760
HimalN <himalnimsara888@gmail.com>
2024-03-12 15:35:23 +0530

    ->Main Dashboard

--- src/main/java/lk/ijse/controller/dashboardFormContrller.java
+++ src/main/java/lk/ijse/controller/dashboardFormContrller.java
@@ -1,4 +1,61 @@
 package lk.ijse.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
 public class dashboardFormContrller {
+
+    @FXML
+    private JFXButton btnBooks;
+
+    @FXML
+    private JFXButton btnBranchers;
+
+    @FXML
+    private JFXButton btnCustomer;
+
+    @FXML
+    private JFXButton btnDashboard;
+
+    @FXML
+    private JFXButton btnLogout;
+
+    @FXML
+    private JFXButton btnRentBooks;
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    void btnBooksOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnBranchesOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCustomerOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDashboardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLogoutOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRentBooksOnAction(ActionEvent event) {
+
+    }
+
 }
--- src/main/java/lk/ijse/launcher.java
+++ src/main/java/lk/ijse/launcher.java
@@ -1,7 +1,32 @@
 package lk.ijse;
 
-public class launcher {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class launcher extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent parent = FXMLLoader.load(getClass().getResource("/view/loginForm.fxml"));
+        Scene scene = new Scene(parent);
+        // Load the icon image
+        Image icon = new Image(getClass().getResourceAsStream("/images/bIcon.png"));
+        // Set the taskbar icon
+        stage.getIcons().add(icon);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.setTitle("Login");
+        stage.show();
+
+
+
+
     }
 }
